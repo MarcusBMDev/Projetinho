d131c6eab691e1f9cdae9b949c4d7ab492b0490d
MarcusBMDev <donmarcola26@gmail.com>
2023-10-17 20:32:44 -0300

    Update AtividadeEmGrupo.py
    Removido a opção 5 que era adicionar e remover do menu.

--- AtividadeEmGrupo.py
+++ AtividadeEmGrupo.py
@@ -94,13 +94,6 @@ while True:
     elif opc == 4:
         indice = int(input("Digite o ID que deseja deletar:"))
         deletar_produto(produtos,indice)
-    elif opc == 5:
-        print("Escolha uma opção:")
-        print("1 - Adicionar;")
-        print("2 - Remover;")
-        opc = int(input(""))
-        indice = int(input("Digite o ID do produto que deseja atualizar:"))
-        atualizar_estoque(produtos, indice)
     else:
 
         break
